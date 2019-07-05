f78764ec391b0cc5a2301e504ee322c898f93169
Chris <chris@bridge.ufsc.br>
2019-07-05 17:06:56 -0300

    configs iniciais do projeto

---
+++ backend/src/test/java/br/ufsc/bridge/cursojunit/CursoJunitApplicationTests.java
@@ -0,0 +1,16 @@
+package br.ufsc.bridge.cursojunit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoJunitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
